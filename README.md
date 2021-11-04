fe95e53a4f5ada7912dfe6df8872468f699418f8
HumamProject <fdilumam@gmail.com>
2021-11-04 21:46:05 +0700

    Luas dan Keliling Lingkaran

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Pratikum-2
+
+Luas Lingkaran
+
+Diketahui = a. phi= 3,14
+	    b. r = 10
+
+Ditanya = Luas Lingkaran ?
+Jawab = phi*r*r
+      = 3,14*10*10
+      = 314
+
+Keliling Linkarang
+
+Diketahui = a. phi= 3,14
+	  = b. r = 20
+
+Ditanya = Keliling Lingkaran ?
+Jawab = 2*phi*r
+      = 2*3,14*20
+      = 125,6
---
+++ Tugas Luas dan Keliling Lingkaran/Iqbal.py
@@ -0,0 +1,18 @@
+#nama saya
+print ( "Program Menghitung Luas Lingkaran dan Keliling Lingkarang \n " )
+print ( "Nama Muhammad Iqbal" )
+print ( "NIM 312110500")
+print ( "Tanggal Pengerjaan Tugas 02 November 2021 \n " )
+
+#menghitung luas lingkaran
+phi  =  3.14
+r  = float(input (" Masukan jari jari Lingkaran = " ))
+luas_Lingkaran = phi*r*r
+
+print ( "luas lingkaran adalah = " , luas_Lingkaran )
+
+#menghitung keliling lingkaran
+r  = float(input ( " Masukan jari jari Lingkaran :" ))
+keliling_Lingkaran =  2*phi*r
+
+print ( "keliling lingkaran adalah = " , keliling_Lingkaran )
